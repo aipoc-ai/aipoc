a1e5bf6a062c1ddd45deaa48d81e50b36e9ab29f
Deepanshu tyagi <deepanshubhai84@gmail.com>
2020-10-23 21:07:02 +0530

    Update rotate_wheels.py

---
+++ rotate_wheels.py
@@ -0,0 +1,53 @@
+import RPi.GPIO as GPIO          
+from time import sleep
+
+in1 = 24
+in2 = 23
+in3=  14
+in4= 18
+en = 23
+
+def forward(x):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(14,GPIO.OUT)
+    GPIO.setup(18,GPIO.OUT)    
+    GPIO.output(14,GPIO.HIGH)
+    GPIO.output(18,GPIO.HIGH)
+    sleep(x)
+    GPIO.cleanup()
+    
+def backward(x):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(23,GPIO.OUT)
+    GPIO.setup(24,GPIO.OUT)
+    GPIO.output(23,True)
+    GPIO.output(24,True)
+    sleep(x)
+    GPIO.cleanup()
+
+def rotate_r(x):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(23,GPIO.OUT)
+    GPIO.setup(18,GPIO.OUT)
+    GPIO.output(23,True)
+    GPIO.output(18,True)
+    sleep(x)
+    GPIO.cleanup()
+
+def rotate_l(x):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(24,GPIO.OUT)
+    GPIO.setup(14,GPIO.OUT)
+    GPIO.output(24,GPIO.HIGH)
+    GPIO.output(14,GPIO.HIGH)
+    sleep(x)
+    GPIO.cleanup()
+
+rotate_l(1.1)
+forward(10)
+rotate_r(1.1)
+forward(0.2)
+rotate_l(1.1)
+forward(0.2)
+rotate_r(1.1)
+
