7001ba6505bdb251e21ce5f3d147fb6d6f3626fa
Brillianttyagi <deepanshubhai84@gmail.com>
2020-12-26 17:58:43 +0530

    added weather and temperature functionality to aipoc

--- main_program2.py
+++ main_program2.py
@@ -2,24 +2,32 @@ from main_program import command,speak
 from play_audio import play_music_func
 from time import sleep
 from ques_ans import get_ans
+from get_weather import get_weather
+from remove_stop_words import remove_words
 
 while True:
     speak("Yes sir?")
     x = command()
     #play music
-    if "(play music" in x ) or ("play song" in x):
+    if ("play music" in x) or ("play song" in x):
         speak("please tell me song?")
         query = command()
         t = play_music_func(query)
     
     #get ans
     elif list(x.split(' '))[0] in ['what','who','tell','whose']:
-        speak(get_ans(x))
+        if ("weather" in x) or ("temperature" in x):
+            try:
+                city_name = remove_words(x)
+                temperature,list_ = get_weather(str(city_name))
+                speak("the temperature of "+str(city_name)+ "is "+str(temperature)+" at "+str(list_[0])+" and the weather is "+str(list_[1])+".")
+            except:
+                speak("i don't understand the name of the city please ask again?")
+        else:
+            speak(get_ans(x))
 
-    elif x in "exit":
-        break
-    elif x in "exit":
-        break
+    elif "sleep" in x:
+        sleep(60)
     elif x in "exit":
         break
     elif x in "exit":
@@ -53,4 +61,4 @@ while True:
             speak("no music is playing. I can play music for you only you have to say play music aipoc")
     
     else:
-        speak("I am learning this right know?")
+        speak("I am learning this right now?")
