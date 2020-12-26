3beb8a9a7f799fbdc621ea8e0774f5d2ff3fe773
Brillianttyagi <deepanshubhai84@gmail.com>
2020-12-26 18:46:52 +0530

    update main program

--- main_program2.py
+++ main_program2.py
@@ -1,70 +1,79 @@
-from main_program import command,speak
+from main_program import command,speak,speak2
 from play_audio import play_music_func
 from time import sleep
 from ques_ans import get_ans
 from get_weather import get_weather
 from remove_stop_words import remove_words
 from basic_question import name_owner
+from send_mail import mail
 
 while True:
-    speak("     Yes    sir?")
-    x = command()
-    #play music
-    if ("play music" in x) or ("play song" in x):
-        speak("please tell me song?")
-        query = command()
-        t = play_music_func(query)
-    
-    #get ans
-    elif list(x.split(' '))[0] in ['what','who','tell','whose']:
-        if ("weather" in x) or ("temperature" in x):
+    try:
+        speak2("hello i am aipoc")
+        speak2("     Yes    sir?")
+        x = command()
+        #play music
+        if ("play music" in x) or ("play song" in x):
+            speak2("please tell me song?")
+            query = command()
+            t = play_music_func(query)
+        
+        #get ans
+        elif list(x.split(' '))[0] in ['what','who','tell','whose']:
+            if ("weather" in x) or ("temperature" in x):
+                try:
+                    city_name = remove_words(x)
+                    temperature,list_ = get_weather(str(city_name))
+                    speak2("the temperature of "+str(city_name)+ "is "+str(temperature)+" at "+str(list_[0])+" and the weather is "+str(list_[1])+".")
+                except:
+                    speak2("i don't understand the name of the city please ask again?")
+            elif ("my name" in x) or ("your ownwer" in x):
+                x = name_owner()
+                speak2(x)
+            else:
+                speak2(get_ans(x))
+
+        elif "wait" in x: 
+            speak2("ok sir?")
+            sleep(60)
+
+        elif "send mail" in x:
+            mail()
+
+        elif x in "exit":
+            break
+        elif x in "exit":
+            break
+        elif x in "exit":
+            break
+        elif x in "exit":
+            break
+        elif x in "exit":
+            break
+        elif x in "exit":
+            break
+        elif x in "exit":
+            break
+        elif x in "exit":
+            break
+        elif x in "exit":
+            break
+        elif x in "exit":
+            break
+        elif x in "exit":
+            break
+        elif x in "exit":
+            break
+        elif ("stop music" in x) or ("stop song" in x):
             try:
-                city_name = remove_words(x)
-                temperature,list_ = get_weather(str(city_name))
-                speak("the temperature of "+str(city_name)+ "is "+str(temperature)+" at "+str(list_[0])+" and the weather is "+str(list_[1])+".")
+                t.kill()
             except:
-                speak("i don't understand the name of the city please ask again?")
-        elif ("my name" in x) or ("your ownwe" in x):
-            x = name_owner()
-            speak(x)
+                speak2("no music is playing. I can play music for you only you have to say play music aipoc")
+        
         else:
-            speak(get_ans(x))
+            speak2("I am learning this right now?")
 
-    elif "sleep" in x:
-        sleep(60)
-
-    elif x in "exit":
+    except:
+        speak2("hello i am aipoc")
+        speak2("there is no internet connection please connect me?")
         break
-    
-    elif x in "exit":
-        break
-    elif x in "exit":
-        break
-    elif x in "exit":
-        break
-    elif x in "exit":
-        break
-    elif x in "exit":
-        break
-    elif x in "exit":
-        break
-    elif x in "exit":
-        break
-    elif x in "exit":
-        break
-    elif x in "exit":
-        break
-    elif x in "exit":
-        break
-    elif x in "exit":
-        break
-    elif x in "exit":
-        break
-    elif ("stop music" in x) or ("stop song" in x):
-        try:
-            t.kill()
-        except:
-            speak("no music is playing. I can play music for you only you have to say play music aipoc")
-    
-    else:
-        speak("I am learning this right now?")
