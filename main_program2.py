0bd1a6d4f5c3416c6d5529071e900c2008def00a
Brillianttyagi <deepanshubhai84@gmail.com>
2020-12-26 17:24:38 +0530

    updated get ans condition and music condition

--- main_program2.py
+++ main_program2.py
@@ -1,19 +1,56 @@
 from main_program import command,speak
 from play_audio import play_music_func
 from time import sleep
+from ques_ans import get_ans
 
 while True:
-    speak("hello sir what do you want?")
+    speak("Yes sir?")
     x = command()
-    if x in "play music":
+    #play music
+    if "(play music" in x ) or ("play song" in x):
         speak("please tell me song?")
         query = command()
         t = play_music_func(query)
-        sleep(20)
+    
+    #get ans
+    elif list(x.split(' '))[0] in ['what','who','tell','whose']:
+        speak(get_ans(x))
+
     elif x in "exit":
         break
-    else:
+    elif x in "exit":
+        break
+    elif x in "exit":
+        break
+    elif x in "exit":
+        break
+    elif x in "exit":
+        break
+    elif x in "exit":
+        break
+    elif x in "exit":
+        break
+    elif x in "exit":
+        break
+    elif x in "exit":
+        break
+    elif x in "exit":
+        break
+    elif x in "exit":
+        break
+    elif x in "exit":
+        break
+    elif x in "exit":
+        break
+    elif x in "exit":
+        break
+    elif x in "exit":
+        break
+    elif ("stop music" in x) or ("stop song" in x):
         try:
             t.kill()
         except:
-            speak("no music is playing")
+            speak("no music is playing. I can play music for you only you have to say play music aipoc")
+    
+    else:
+        speak("I am learning this right know?")
