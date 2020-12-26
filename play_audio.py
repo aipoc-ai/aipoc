89bc465bbab585f672bfa6394166a4ac1a2ab7ad
Brillianttyagi <deepanshubhai84@gmail.com>
2020-12-26 17:03:37 +0530

    add function to run music file in background

---
+++ play_audio.py
@@ -0,0 +1,21 @@
+import subprocess
+import sys
+from time import sleep
+
+def write_query(query):
+    file1 = open("audio_query.txt","r+")
+    file1.truncate(0)
+    file1.close()
+    with open('audio_query.txt','w') as fb:
+        fb.write(query)
+
+def play_music_func(query):
+            
+    write_query(query)
+
+
+
+    cmd = 'pythonw play_spotify.py'
+
+    p= subprocess.Popen(cmd)
+    return p
