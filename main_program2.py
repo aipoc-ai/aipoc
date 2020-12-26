643611a4c8477fc04d496fcce1e4814139217344
Brillianttyagi <deepanshubhai84@gmail.com>
2020-12-26 17:05:53 +0530

    updated main program file functions

--- main_program2.py
+++ main_program2.py
@@ -1,10 +1,19 @@
-import gtts
-import playsound
-import speech_recognition as sr
-import os
-def speak(text,count):
-    txt = gTTS(text = text,lang="en")
-    file_name = "voice"+str(count)+".mp3"
-    txt.save(file_name)
-    playsound.playsound(file_name)
+from main_program import command,speak
+from play_audio import play_music_func
+from time import sleep
 
+while True:
+    speak("hello sir what do you want?")
+    x = command()
+    if x in "play music":
+        speak("please tell me song?")
+        query = command()
+        t = play_music_func(query)
+        sleep(20)
+    elif x in "exit":
+        break
+    else:
+        try:
+            t.kill()
+        except:
+            speak("no music is playing")
