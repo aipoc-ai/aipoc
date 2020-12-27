7cfb1b1a3afe694c79b1cbc8ca081e77ddc58942
Deepanshu tyagi <deepanshubhai84@gmail.com>
2020-12-27 20:56:02 +0530

    Update rotate_servo.py

--- rotate_servo.py
+++ rotate_servo.py
@@ -5,7 +5,7 @@ servoPIN = 16
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+p = GPIO.PWM(servoPIN, 50) # GPIO 16 for PWM with 50Hz
 p.start(5) # Initialization
 try:
   while True:
